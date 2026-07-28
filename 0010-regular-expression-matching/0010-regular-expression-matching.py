3335a38e16bfe4cb361536c26366a3797924a76f
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-07-28 15:36:24 +0530

    Time: 11 ms (22.9%), Space: 19.7 MB (38.05%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -21,6 +21,31 @@ class Solution:
             else:
                 return first_match and  x(i+1,j+1)
 
-        return x(0,0)
+
+        dp=[[False for _ in range(m+1)]for _ in range(n+1)]
+        dp[n][m]=True
+        
+        for j in range(m - 2, -1, -1):
+            if p[j + 1] == '*':
+                dp[n][j] = dp[n][j + 2]
+
+        for i in range(n-1,-1,-1):
+
+            for j in range(m-1,-1,-1):
+                first_match=i<n and (s[i]==p[j] or p[j]=='.') 
+
+                if j+1<m and (p[j+1]=='*'):
+                    n_take=dp[i][j+2]
+
+                    take=first_match and dp[i+1][j]
+                    dp[i][j]= take or n_take
+                
+                else:
+                    dp[i][j]= first_match and  dp[i+1][j+1]
+
+        return dp[0][0]
+
+
+
 
             
