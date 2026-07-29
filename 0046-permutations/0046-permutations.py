6a7e6d937e35d1eefb6af14988423640c2927d76
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-07-29 13:57:34 +0530

    Time: 223 ms (1.48%), Space: 19.5 MB (65.27%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n=len(nums)
+        h_map=[0]*n
+        ans=[]
+        def x(i,h_map,st):
+            if i==n:
+                if len(st)==n:
+                    ans.append(st[:])
+                return 
+            
+            for v in range(n):
+                if h_map[v]==0:
+                    x(i+1,h_map,st)
+                    st.append(nums[v])
+                    
+                    h_map[v]=1
+                    x(i+1,h_map,st)
+                    h_map[v]=0
+                    st.pop()
+
+            return 
+
+        x(0,h_map,[])
+        return ans
