56dbee00e8c0fda48827a2443d3655fbf18ef054
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-08-01 12:40:23 +0530

    Time: 0 ms (100%), Space: 19.7 MB (36.36%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,7 +1,7 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        n=len(nums)
-        dp=[[-1 for _ in range(n)]for _ in range(n)]
+        n=len(nums )
+        dp=[[0 for _ in range(n)]for _ in range(n)]
 
         def x(i,j):
             if i==j:
@@ -17,6 +17,15 @@ class Solution:
             dp[i][j]= max(take_i,take_j)
             return dp[i][j]
 
-        return x(0,len(nums)-1)>=0
+        for i in range(n):
+            dp[i][i]=nums[i]
+        for i in range(n-2,-1,-1):
+            for j in range(i+1,n):
+                take_i=nums[i]-dp[i+1][j]
+
+                take_j=nums[j]-dp[i][j-1]
+
+                dp[i][j]=max(take_i,take_j)
+        return dp[0][n-1]>=0
 
         
