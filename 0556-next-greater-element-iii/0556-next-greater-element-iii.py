fd081bd922160f53942a81bca3e20b3e19adf8d2
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-08-15 01:05:52 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.24%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.py
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -1,54 +1,18 @@
 class Solution:
-    def nextGreaterElement(self, n: int) -> int:
-        orig=n
-        #i am converting the n into a list containing digits of n
-        def func(arr,x,y):
-            i=x
-            j=y
-            while i<j:
-                temp=arr[i]
-                arr[i]=arr[j]
-                arr[j]=temp
-
-                i+=1
-                j-=1
-            return 
-        nums=[]
-        mult=10
-        while n:
-            nums.append(n%10)
-            n//=10
-        nums.reverse()
-        if len(nums)<=1:
-            return -1
-
-        ind=-1
-
-        for i in range(len(nums)-2,-1,-1):
-            if nums[i]<nums[i+1]:
-                for j in range(len(nums)-1,-1,-1):
-                    if nums[j]>nums[i]:
-                        temp=nums[j]
-                        nums[j]=nums[i]
-                        nums[i]=temp
-
-                        break
-                ind=i
-
-                break
-        if ind == -1:
-            return -1
-
-        func(nums, ind + 1, len(nums) - 1)
-
-        ans = 0
-        mult = 1
-
-        for i in range(len(nums) - 1, -1, -1):
-            ans += nums[i] * mult
-            mult *= 10
-
-        if ans > 2**31 - 1:
-            return -1
-
-        return ans
+    def nextGreaterElement(self, n):
+        digits = list(str(n))
+        i = len(digits) - 1
+        while i-1 >= 0 and digits[i] <= digits[i-1]:
+            i -= 1
+            
+        if i == 0: return -1
+        
+        j = i
+        while j+1 < len(digits) and digits[j+1] > digits[i-1]:
+            j += 1
+        
+        digits[i-1], digits[j] = digits[j], digits[i-1]
+        digits[i:] = digits[i:][::-1]
+        ret = int(''.join(digits))
+        
+        return ret if ret < 1<<31 else -1
