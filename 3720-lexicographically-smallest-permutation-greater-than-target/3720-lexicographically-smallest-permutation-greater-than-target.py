6a78ec6bbae325c1f3236b7488b93905f39235a5
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-08-28 16:07:45 +0530

    Time: 15 ms (51.32%), Space: 19.2 MB (90.79%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,42 +1,42 @@
 class Solution:
+    ans=""
+    
+    #now we try to make permutation just greater than target
+    #we will try to find letter >= for every ith element
+    def solve(self,ind,greater,res,alpha,target,n):
+        if ind==n:
+            if greater:
+                self.ans=''.join(res)
+                return True
+            return False
+
+        for i in range(0,26):
+            if alpha[i]==0 :continue
+
+            if not greater and chr(i+ord('a'))<target[ind]:
+                continue
+
+            res.append(chr(i+ord('a')))
+            alpha[i]-=1
+
+            is_greater=greater or chr(i+ord('a'))>target[ind]
+
+            if self.solve(ind+1,is_greater,res,alpha,target,n):
+                return True
+
+            res.pop()
+            alpha[i]+=1
+
+        return False
+
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         n=len(s)
         alpha=[0]*26
         for i in s:
             alpha[ord(i)-ord('a')]+=1
 
-        
-        
-        #now we try to make permutation just greater than target
-        #we will try to find letter >= for every ith element
-        ans=[""]
-        def x(ind,greater,res,alpha,ans):
-            if ind==n:
-                if greater:
-                    ans[0]=''.join(res)
-                    return True
-                return False
-
-            for i in range(0,26):
-                if alpha[i]==0 :continue
-
-                if not greater and chr(i+ord('a'))<target[ind]:
-                    continue
-
-                res.append(chr(i+ord('a')))
-                alpha[i]-=1
-
-                is_greater=greater or chr(i+ord('a'))>target[ind]
-
-                if x(ind+1,is_greater,res,alpha,ans):
-                    return True
-
-                res.pop()
-                alpha[i]+=1
-
-            return False
-        x(0,False,[],alpha,ans)
-        return ans[0]
+        self.solve(0,False,[],alpha,target,n)
+        return self.ans
 
                 
 
