2fc70b316ce83636456bd14f901c4621e1a7f9a3
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-09-05 18:56:03 +0530

    Time: 6 ms (45.87%), Space: 21.9 MB (78.04%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -4,23 +4,63 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class BST_iterator:
+    #reverse=true==next
+    #reverse=false==before
+
+    def __init__(self,root,isreverse):
+        self.reverse=isreverse
+        self.st=[]
+        self.func(root)
+
+    def hasnext(self):
+        return len(self.st)!=0
+
+    def next(self):
+        if self.reverse:
+            tmp=self.st.pop()
+            self.func(tmp.right)
+            return tmp.val
+        else:
+            tmp=self.st.pop()
+            self.func(tmp.left)
+            return tmp.val
+
+    def func(self,node):
+        if self.reverse:
+            while node:
+                self.st.append(node)
+                node=node.left
+        else:
+            while node:
+                self.st.append(node)
+                node=node.right
+
+
+
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        vis=set()
-        st=[]
-        while root or st:
-            while root:
-                st.append(root)
-                root=root.left
 
-            root=st.pop()
-            
-            if k-(root.val)in vis:
+        i = BST_iterator(root, True)
+        j = BST_iterator(root, False)
+
+        left = i.next()
+        right = j.next()
+
+        while left < right:
+            if left + right == k:
                 return True
-            vis.add(root.val)
 
-            root=root.right
+            if left + right < k:
+                if i.hasnext():
+                    left = i.next()
+                else:
+                    break
+            else:
+                if j.hasnext():
+                    right = j.next()
+                else:
+                    break
 
         return False
-            
-        
+
