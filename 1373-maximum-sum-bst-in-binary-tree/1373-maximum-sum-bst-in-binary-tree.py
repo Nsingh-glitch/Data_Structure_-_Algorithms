db160d058ac186f61f90a97534c98cd130feb91b
Nikhil Singh <nikhilsingh8265@gmail.com>
2026-09-06 16:27:34 +0530

    Time: 75 ms (90.49%), Space: 44.8 MB (87.02%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.py
@@ -7,18 +7,18 @@ class Solution:
         def x(root):
             nonlocal ans
             if not root:
-                return (0, int_max, int_min, True)
+                return (0, int_max, int_min)
 
-            s1, l_mini, l_maxi, lb = x(root.left)
-            s2, r_mini, r_maxi, rb = x(root.right)
+            s1, l_mini, l_maxi = x(root.left)
+            s2, r_mini, r_maxi = x(root.right)
 
-            if lb and rb and root.val > l_maxi and r_mini > root.val:
+            if  root.val > l_maxi and r_mini > root.val:
                 tmp = s1 + s2 + root.val
                 ans=max(ans,tmp)
-                return (tmp, min(l_mini, root.val), max(r_maxi, root.val), True)
+                return (tmp, min(l_mini, root.val), max(r_maxi, root.val))
 
             else:
-                return (0, int_max, int_min, False)
+                return (0, int_min, int_max)
 
  
 
